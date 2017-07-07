caab265774b0944843c54f24f7394a7da890254c
Ahaoplus <596111492@qq.com>
2017-07-07 17:47:28 +0800

    简易版完成
    1、加载天气信息
    2、添加每日一图

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,13 +1,236 @@
 package com.coolweather.android;
 
+import android.content.SharedPreferences;
+import android.media.Image;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.coolweather.android.gson.Forecast;
+import com.coolweather.android.gson.Weather;
+import com.coolweather.android.util.HttpUtil;
+import com.coolweather.android.util.Utility;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    private ScrollView weatherLayout;
+
+    private TextView titleCity;
+
+    private TextView titleUpdateTime;
+
+    private TextView degreeText;
+
+    private TextView weatherInfoText;
+
+    private LinearLayout forecastLayout;
+
+    private TextView aqiText;
+
+    private TextView pm25Text;
+
+    private TextView comfortText;
+
+    private TextView carWashText;
+
+    private TextView sportText;
+
+    private ImageView bingPicImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        //初始化各控件
+        weatherLayout = (ScrollView)findViewById(R.id.weather_layout);
+        titleCity = (TextView)findViewById(R.id.title_city);
+        titleUpdateTime = (TextView)findViewById(R.id.title_update_time);
+        degreeText = (TextView)findViewById(R.id.degree_text);
+        weatherInfoText = (TextView)findViewById(R.id.weather_info_text);
+        forecastLayout = (LinearLayout)findViewById(R.id.forecast_layout);
+        aqiText = (TextView)findViewById(R.id.aqi_text);
+        pm25Text= (TextView)findViewById(R.id.pm25_text);
+        comfortText = (TextView)findViewById(R.id.confort_text);
+        carWashText = (TextView)findViewById(R.id.car_wash_text);
+        sportText = (TextView)findViewById(R.id.sport_text);
+        bingPicImg = (ImageView)findViewById(R.id.bing_pic_img);
+
+        //数据缓存
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String weatherString = prefs.getString("weather",null);
+        //有缓存是直接解析天气数据
+        if (weatherString !=null){
+            Weather weather = Utility.handleWeatherResponse(weatherString);
+            showWeatherInfo(weather);
+        }else{
+
+            //无缓存时去服务器查询天气
+            String weatherId = getIntent().getStringExtra("weather_id");
+            weatherLayout.setVisibility(View.INVISIBLE);
+            requestWeather(weatherId);
+        }
+
+        //获取bing每日一图
+        String bingPic = prefs.getString("bing_pic",null);
+
+        if (bingPic !=null){
+
+            Glide.with(this).load(bingPic).into(bingPicImg);
+
+        }else{
+
+            loadBingPic();
+        }
+
+    }
+    /**
+     * 根据天气id请求城市天气信息
+     * */
+    public  void requestWeather(final String weatherId){
+
+        loadBingPic();
+
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" +
+                weatherId +
+                "&key=bc0418b57b2d4918819d3974ac1285d9";
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                final Weather weather = Utility.handleWeatherResponse(responseText);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (weather !=null && "ok".equals(weather.status)){
+
+                            /*
+                            * 缓存数据
+                            * SharedPreferences是Android平台上一个轻量级的存储类，
+                            * 主要是保存一些常用的配置比如窗口状态，一般在Activity中
+                            * 重载窗口状态onSaveInstanceState保存一般使用SharedPreferences完成，
+                            * 它提供了android平台常规的Long长整形、Int整形、String字符串型的保存，
+                            * 它是什么样的处理方式呢?
+                            * SharedPreferences类似过去Windows系统上的ini配置文件，
+                            * 但是它分为多种权限，可以全局共享访问，android123提示最终是以xml方式来保存，
+                            * 整体效率来看不是特别的高，对于常规的轻量级而言比SQLite要好不少，
+                            * 如果真的存储量不大可以考虑自己定义文件格式。xml处理时Dalvik会通过自带底层的
+                            * 本地XML Parser解析，比如XMLpull方式，这样对于内存资源占用比较好。
+                            * */
+                            SharedPreferences.Editor editor = PreferenceManager.
+                                    getDefaultSharedPreferences(WeatherActivity.this).edit();
+                            editor.putString("weather",responseText);
+                            editor.apply();
+                            showWeatherInfo(weather);
+                        }else{
+
+                            Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void showWeatherInfo(Weather weather){
+
+        String cityName = weather.basic.cityName;
+        String updateTime = weather.basic.update.updateTime.split(" ")[1];
+        String degree = weather.now.temperature+"摄氏度";
+        String weatherInfo = weather.now.more.info;
+        titleCity.setText(cityName);
+        titleUpdateTime.setText(updateTime);
+        degreeText.setText(degree);
+        weatherInfoText.setText(weatherInfo);
+        forecastLayout.removeAllViews();
+        for (Forecast forecast : weather.forecastList){
+
+            View view = LayoutInflater.from(this).inflate(R.layout.forecast_item,forecastLayout,false);
+
+            TextView dateText = (TextView) view.findViewById(R.id.date_text);
+            dateText.setText(forecast.date);
+            TextView infoText = (TextView) view.findViewById(R.id.info_text);
+            infoText.setText(forecast.more.info);
+            TextView maxText = (TextView) view.findViewById(R.id.max_text);
+            maxText.setText(forecast.temperature.max);
+            TextView minText = (TextView) view.findViewById(R.id.min_text);
+            minText.setText(forecast.temperature.min);
+            forecastLayout.addView(view);
+
+        }
+
+        if (weather.aqi !=null){
+
+            aqiText.setText(weather.aqi.city.aqi);
+            pm25Text.setText(weather.aqi.city.pm25);
+        }
+
+        String comfort = "舒适度："+weather.suggestion.comfort.info;
+        String carWash = "洗车指数："+weather.suggestion.carWash.info;
+        String sport = "运动建议："+weather.suggestion.sport.info;
+
+        comfortText.setText(comfort);
+        carWashText.setText(carWash);
+        sportText.setText(sport);
+
+        weatherLayout.setVisibility(View.VISIBLE);
+    }
+
+    private void loadBingPic(){
+
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic",bingPic);
+
+                editor.apply();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 
 import org.json.JSONArray;
@@ -97,5 +99,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    //alt+enter键
+    public static Weather handleWeatherResponse(String response){
+
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
